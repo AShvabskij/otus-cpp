2c3b44dd8d1af2ebc6fddfaf17cdc62d174640b7
Alexander Shvabsky <ashvabsky@gmail.com>
2026-08-28 14:36:14 +0300

    added google tests

---
+++ matrix.h
@@ -0,0 +1,127 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include <map>
+#include <tuple>
+
+template <typename T, T DefaultValue>
+class Matrix {
+    std::map<std::pair<size_t /*row*/, size_t/*col*/>, T> m_data;
+    size_t m_occupied_count = 0;
+
+    class Cell {
+        Matrix<T, DefaultValue>& m_matrix;
+        size_t m_row = 0;
+        size_t m_col = 0;
+
+    public:
+        Cell(Matrix<T, DefaultValue>& m, size_t r, size_t c): m_matrix(m), m_row(r), m_col(c) {}
+
+        operator T() const {
+            auto key = std::make_pair(m_row, m_col);
+            auto it = m_matrix.m_data.find(key);
+            if (it != m_matrix.m_data.end()) {
+                return it->second;
+            } else {
+                return DefaultValue;
+            }
+        }
+
+        Cell& operator= (const T& value) {
+            auto key = std::make_pair(m_row, m_col);
+            auto it = m_matrix.m_data.find(key);
+            bool is_found = (it != m_matrix.m_data.end());
+
+            if (value == DefaultValue ) {
+                if (is_found) {
+                    m_matrix.m_data.erase(it);
+                    m_matrix.m_occupied_count--;
+                }
+            } else {
+                m_matrix.m_data[key] = value;
+                if (!is_found) {
+                    m_matrix.m_occupied_count++;
+                }
+            }
+
+            return *this;
+        }
+
+        Cell& operator= (const Cell& other) {
+            T value = static_cast<T>(other);
+            return *this;
+        }
+    };
+
+    class Row {
+    public:
+        Row(Matrix<T, DefaultValue>& m, size_t r) : m_matrix(m), m_row(r) {}
+
+        Cell operator[](size_t col) {
+            return Cell(m_matrix, m_row, col);
+        };
+
+    private:
+        Matrix& m_matrix;
+        size_t m_row = 0;
+    };
+
+public:
+    Matrix() = default;
+
+    Row operator[](size_t row) {
+        return Row(*this, row);
+    }
+
+    size_t size() {
+        return m_occupied_count;
+    }
+
+    class Iterator
+    {
+    private:
+        typename std::map<std::pair<size_t /*row*/, size_t/*col*/>, T>::const_iterator m_it;
+
+    public:
+        Iterator(typename std::map<std::pair<size_t /*row*/, size_t/*col*/>, T>::const_iterator it) : m_it(it) {}
+
+        Iterator& operator++() {
+            m_it++;
+            return *this;
+        }
+
+        bool operator !=(const Iterator& other_it) const {
+            return m_it != other_it.m_it;
+        }
+
+        std::tuple<size_t, size_t, T> operator*() const {
+            return std::make_tuple(m_it->first.first, m_it->first.second, m_it->second);
+        }
+    };
+
+    Iterator begin() const {
+        return Iterator(m_data.begin());
+    }
+
+    Iterator end() const {
+        return Iterator(m_data.end());
+    }
+
+    // Method to display matrix fragment
+    void printFragment(size_t start_row, size_t end_row, size_t start_col, size_t end_col) {
+        for (size_t i = start_row; i <= end_row; ++i) {
+            for (size_t j = start_col; j <= end_col; ++j) {
+                auto key = std::make_pair(i, j);
+                auto it = m_data.find(key);
+                T val = (it != m_data.end()) ? it->second : DefaultValue;
+                std::cout << val;
+                if (j < end_col) std::cout << " ";
+            }
+
+            std::cout << std::endl;
+        }
+    }
+
+};
+
+#endif // MATRIX_H
---
+++ tests/test_matrix.cpp
@@ -0,0 +1,113 @@
+#include <gtest/gtest.h>
+#include "../matrix.h" // или вынесите класс Matrix в отдельный заголовочный файл
+
+// Тест 1: Создание матрицы и проверка размера
+TEST(MatrixTest, DefaultConstruction) {
+    Matrix<int, 0> matrix;
+    EXPECT_EQ(matrix.size(), 0);
+}
+
+// Тест 2: Установка и получение значений
+TEST(MatrixTest, SetAndGetValue) {
+    Matrix<int, 0> matrix;
+    matrix[0][0] = 42;
+    EXPECT_EQ(matrix[0][0], 42);
+    EXPECT_EQ(matrix.size(), 1);
+}
+
+// Тест 3: Значение по умолчанию
+TEST(MatrixTest, DefaultValue) {
+    Matrix<int, -1> matrix;
+    EXPECT_EQ(matrix[10][20], -1);
+    EXPECT_EQ(matrix.size(), 0);
+}
+
+// Тест 4: Удаление элементов при установке значения по умолчанию
+TEST(MatrixTest, RemoveElementWhenSetToDefault) {
+    Matrix<int, 0> matrix;
+    matrix[5][5] = 100;
+    EXPECT_EQ(matrix.size(), 1);
+
+    matrix[5][5] = 0; // Устанавливаем значение по умолчанию
+    EXPECT_EQ(matrix[5][5], 0);
+    EXPECT_EQ(matrix.size(), 0);
+}
+
+// Тест 5: Диагональная матрица
+TEST(MatrixTest, DiagonalMatrix) {
+    Matrix<int, 0> matrix;
+    for (int i = 0; i < 10; i++) {
+        matrix[i][i] = i;
+        matrix[i][9-i] = 9-i;
+    }
+    EXPECT_EQ(matrix.size(), 18);
+}
+
+// Тест 6: Проверка оператора присваивания Cell
+TEST(MatrixTest, CellAssignmentOperator) {
+    Matrix<int, 0> matrix;
+    matrix[0][0] = 5;
+    matrix[1][1] = 10;
+
+    // Проверяем, что значения установлены правильно
+    EXPECT_EQ(matrix[0][0], 5);
+    EXPECT_EQ(matrix[1][1], 10);
+    EXPECT_EQ(matrix.size(), 2);
+}
+
+// Тест 7: Проверка канонической формы (цепочки присваиваний)
+TEST(MatrixTest, CanonicalForm) {
+    Matrix<int, -1> matrix;
+    ((matrix[100][100] = 314) = 0) = 217;
+    EXPECT_EQ(matrix[100][100], 217);
+    EXPECT_EQ(matrix.size(), 1);
+}
+
+// Тест 8: Итерация по матрице
+TEST(MatrixTest, Iterator) {
+    Matrix<int, 0> matrix;
+    matrix[0][0] = 1;
+    matrix[1][1] = 2;
+    matrix[2][2] = 3;
+
+    int count = 0;
+    int sum = 0;
+    for (auto [x, y, v] : matrix) {
+        count++;
+        sum += v;
+    }
+    EXPECT_EQ(count, 3);
+    EXPECT_EQ(sum, 6);
+}
+
+// Тест 9: Перезапись существующего значения
+TEST(MatrixTest, OverwriteValue) {
+    Matrix<int, 0> matrix;
+    matrix[0][0] = 10;
+    EXPECT_EQ(matrix.size(), 1);
+
+    matrix[0][0] = 20;
+    EXPECT_EQ(matrix[0][0], 20);
+    EXPECT_EQ(matrix.size(), 1); // Размер не должен измениться
+}
+
+// Тест 10: Множество различных значений
+TEST(MatrixTest, MultipleValues) {
+    Matrix<int, -1> matrix;
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            matrix[i][j] = i * 10 + j;
+        }
+    }
+
+    EXPECT_EQ(matrix.size(), 25);
+    EXPECT_EQ(matrix[2][3], 23);
+    EXPECT_EQ(matrix[4][4], 44);
+}
+
+/*
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+*/
